470c5c15b85aa269f9500d463e3e7246fd41e3e2
Rob Anderson <roboprog@yahoo.com>
2013-06-05 13:23:41 -0700

    start on "Write" mode of demo

--- ex1.groovy
+++ ex1.groovy
@@ -4,7 +4,19 @@
 
 switch ( args[ 0 ]) {
 	case "W" :
-			println "TODO: write"
+			// init some data
+			def aMap = [ "a" : 42, "b" : "random" ]
+			def anotherMap = [ "a" : 86, "b" : "text" ]
+
+			// make some circular references
+			aMap[ "buddy" ] = anotherMap
+			anotherMap[ "buddy" ] = aMap
+
+			// get a stack overflow:
+			println aMap
+
+			// dump
+			println "TODO: write " + aMap
 		break
 	case "R" :
 			println "TODO: read"

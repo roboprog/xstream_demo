5d49613c127e651367d57502df1569acb199bd45
Rob Anderson <roboprog@yahoo.com>
2013-06-05 13:18:23 -0700

    stub for example 1

---
+++ ex1.groovy
@@ -0,0 +1,17 @@
+
+// read or write some data, use XStream library to [de]serialize as needed,
+//  using XML encoding.
+
+switch ( args[ 0 ]) {
+	case "W" :
+			println "TODO: write"
+		break
+	case "R" :
+			println "TODO: read"
+		break
+	default :
+			println "Huh?"
+		break
+}
+
+

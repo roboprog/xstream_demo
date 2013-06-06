976641c318b70a4f2d74a0d3cf3ea3a2c5ff75e9
Rob Anderson <roboprog@yahoo.com>
2013-06-06 13:48:10 -0700

    read back XML and reconstitute object(s)

--- ex1.groovy
+++ ex1.groovy
@@ -2,7 +2,8 @@
 // read or write some data, use XStream library to [de]serialize as needed,
 //  using XML encoding.
 
-codec = new com.thoughtworks.xstream.XStream()
+codec = new com.thoughtworks.xstream.XStream(
+		new com.thoughtworks.xstream.io.xml.DomDriver() )
 
 switch ( args[ 0 ]) {
 	case "W" :
@@ -21,7 +22,17 @@ switch ( args[ 0 ]) {
 			println codec.toXML( aMap)
 		break
 	case "R" :
-			println "TODO: read"
+			// load object back (from stdin)
+			def inStr = System.in.text
+			def aMap = (Map) codec.fromXML( inStr)
+
+			// clip out the circular references
+			def anotherMap = (Map) aMap[ "buddy" ]
+			aMap.remove( "buddy")
+			anotherMap.remove( "buddy")
+
+			// open the cabinet to show the result of the magic trick:
+			println "The maps, (mostly) unharmed: \n\t1) ${aMap}; \n\t2) ${anotherMap}"
 		break
 	default :
 			println "Huh?"
